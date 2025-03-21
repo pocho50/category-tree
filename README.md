830eb3ce7c69f720f254f4bd6288a1e6e9aa42af
Jose Luis <cabrera@inkuba.com>
2025-03-21 15:26:58 +0100

    feat: add title to the page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Nuxt Minimal Starter
+# Categories Tree
 
 Look at the [Nuxt documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon"],
+  app: {
+    head: {
+      title: "Categories tree",
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
